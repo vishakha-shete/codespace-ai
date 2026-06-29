65d40edf3846a955ae34e574aecc1326c3d6cc4f
vishakha shete <vishakhashete2@gmail.com>
2026-06-29 22:48:55 +0530

    feat: implement Kubernetes sandbox environment provisioning

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,19 +1,44 @@
 import express from "express";
 import morgan from "morgan";
-
+import { createPod } from "./kuberneties/pod.js";
+import { createService } from "./kuberneties/service.js";
+import { v7 as uuid } from "uuid"
 
 const app = express();
 
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/sandbox/health', (req, res)=>{
+app.get('/api/sandbox/health', (req, res) => {
     res.status(200).json({
         message: 'sandbox API is healthy',
         status: 'ok'
     })
 });
+
+app.post("/api/sandbox/start", async (req, res) => {
+    try {
+        const sandboxId = uuid();
+
+        await Promise.all([
+            createPod(sandboxId),
+            createService(sandboxId)
+        ]);
+
+        return res.status(201).json({
+            message: "Sandbox environment created successfully",
+            previewUrl: `http://${sandboxId}.localhost`
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Failed to create sandbox environment"
+        });
+    }
+})
 
 export default app;
---
+++ sandbox/server/src/kuberneties/pod.js
@@ -0,0 +1,47 @@
+import { k8sCoreV1Api } from "./config.js";
+
+export async function createPod(sandboxId) {
+  const podManifest = {
+    apiVersion: "v1",
+    kind: "Pod",
+    metadata: {
+      name: `sandbox-pod-${sandboxId}`,
+      labels: {
+        app: "sandbox",
+        sandboxId: sandboxId,
+      },
+    },
+    spec: {
+      containers: [
+        {
+          name: "sandbox-container",
+          image: "template",
+          imagePullPolicy: "IfNotPresent",
+          ports: [
+            {
+              containerPort: 5173,
+              name: "http",
+            },
+          ],
+          resources: {
+            limits: {
+              cpu: "500m",
+              memory: "1Gi",
+            },
+            requests: {
+              cpu: "250m",
+              memory: "500Mi",
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  const response = await k8sCoreV1Api.createNamespacedPod({
+    namespace: "default",
+    body: podManifest,
+  });
+
+  return response;
+}
