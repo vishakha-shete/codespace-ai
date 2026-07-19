b1aabf9ff7fdb3332ba94c495f5a171d356e0ea2
vishakha shete <vishakhashete2@gmail.com>
2026-07-19 22:56:34 +0530

    feat: add /read-files endpoint to read and return file contents from workspace

--- sandbox/agent/src/app.js
+++ sandbox/agent/src/app.js
@@ -25,4 +25,56 @@ app.get("/list-files", async (req, res) => {
     });
 });
 
+
+/**
+ * @route GET /read-files
+ * @description  Reads the content of all files requested in the query parameter 'files' and return their content as a json object
+ * - e.g /read-files?files=filel.txt,/src/file2.txt
+ * 
+ */
+app.get("/read-files", async (req, res) => {
+
+    const files = req.query.files;
+
+    if (!files) {
+        return res.status(400).json({
+            message: "No files specified in query parameter",
+            status: "error",
+        });
+    }
+
+    const fileList = files.split(",");
+
+    const results = await Promise.all(
+        fileList.map(async (file) => {
+
+            const filePath = `${WORKSPACE_DIR}/${file}`;
+
+            try {
+
+                const content = await fs.promises.readFile(filePath, "utf-8");
+
+                return {
+                    [file]: content,
+                };
+
+            } catch (err) {
+
+                return {
+                    [file]: `Error reading file: ${err.message}`,
+                };
+
+            }
+
+        })
+    );
+
+    return res.status(200).json({
+        message: "Files read successfully",
+        files: Object.assign({}, ...results),
+        status: "success",
+    });
+
+});
+
 export default app;
